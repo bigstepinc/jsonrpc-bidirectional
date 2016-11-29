7ec22119b61a01e0d35fb0fa847f2b71696b0be1
Ovidiu Giorgi <ovidiu.giorgi@bigstep.com>
2016-11-29 13:15:16 +0200

    Move index.js to directory's root

---
+++ index.js
@@ -0,0 +1,15 @@
+require("babel-core/register");
+require("babel-polyfill");
+
+const obj={};
+obj.JSONRPC={};
+
+obj.JSONRPC.Client=require("./src/Client");
+obj.JSONRPC.ClientFilterBase=require("./src/ClientFilterBase");
+obj.JSONRPC.Exception=require("./src/Exception");
+obj.JSONRPC.Utils=require("./src/Utils.js");
+
+obj.JSONRPC.Filter=obj.JSONRPC.Filter || {};
+obj.JSONRPC.Filter.Client=require("./src/filters/client/index");
+
+module.exports=obj;
