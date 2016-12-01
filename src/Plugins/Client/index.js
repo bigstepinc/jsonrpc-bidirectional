84ebdab6f6edeb0271797f7b1b79a639fb0e823d
ionut.stan <ionut.stan@bigstep.com>
2016-12-01 23:31:58 +0200

    The way it is done now.

--- src/Plugins/Client/index.js
+++ src/Plugins/Client/index.js
@@ -3,5 +3,6 @@ const obj={};
 obj.DebugLogger=require("./DebugLogger");
 obj.PrettyBrowserConsoleErrors=require("./PrettyBrowserConsoleErrors");
 obj.SignatureAdd=require("./SignatureAdd");
+obj.WebSocketTransport=require("./WebSocketTransport");
 
 module.exports=obj;
