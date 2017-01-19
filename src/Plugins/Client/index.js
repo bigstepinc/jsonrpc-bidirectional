ae5e9e048404d7390bacf5e2c358509e25ed2381
ionut.stan <ionut.stan@bigstep.com>
2017-01-19 22:38:54 +0200

    Forgot to export ProcessStdIOTransport.

--- src/Plugins/Client/index.js
+++ src/Plugins/Client/index.js
@@ -4,5 +4,6 @@ objExports.DebugLogger = require("./DebugLogger");
 objExports.PrettyBrowserConsoleErrors = require("./PrettyBrowserConsoleErrors");
 objExports.SignatureAdd = require("./SignatureAdd");
 objExports.WebSocketTransport = require("./WebSocketTransport");
+objExports.ProcessStdIOTransport = require("./ProcessStdIOTransport");
 
 module.exports = objExports;
