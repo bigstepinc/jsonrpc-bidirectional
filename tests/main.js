edcd660d25147cc3de30955f5f50459db3fb36f7
ionut.stan <ionut.stan@bigstep.com>
2017-04-25 23:34:50 +0300

    Commented uws tests in tests/main.js

--- tests/main.js
+++ tests/main.js
@@ -19,9 +19,9 @@ process.on(
 		let allTests = new AllTests(bBenchmarkMode, /*bWebSocketMode*/ false);
 		await allTests.runTests();
 
-		allTests = new AllTests(bBenchmarkMode, /*bWebSocketMode*/ true, require("uws"), require("uws").Server, JSONRPC.WebSocketAdapters.uws.WebSocketWrapper, /*bDisableVeryLargePacket*/ true);
-		allTests.websocketServerPort = allTests.httpServerPort + 1;
-		await allTests.runTests();
+		//allTests = new AllTests(bBenchmarkMode, /*bWebSocketMode*/ true, require("uws"), require("uws").Server, JSONRPC.WebSocketAdapters.uws.WebSocketWrapper, /*bDisableVeryLargePacket*/ true);
+		//allTests.websocketServerPort = allTests.httpServerPort + 1;
+		//await allTests.runTests();
 
 		allTests = new AllTests(bBenchmarkMode, /*bWebSocketMode*/ true, require("ws"), require("ws").Server, undefined, /*bDisableVeryLargePacket*/ false);
 		await allTests.runTests();
